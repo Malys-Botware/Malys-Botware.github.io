ba767d61bb4d0b012a90bf5a889fdbd65eae495f
Malys <ba1thazar.23730@gmail.com>
2026-03-22 15:17:39 -0600

    Update sw.js

--- journal/sw.js
+++ journal/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'malys-journal-v2';
+const CACHE = 'malys-journal-v4';
 const CORE = ['./', './index.html', './manifest.json', './icon.svg'];
 const CDN = [
   'https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=IBM+Plex+Mono:wght@300;400;500&display=swap',
