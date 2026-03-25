6de09fd7d7054d77d6fca32b3d6bc1fdd86cafad
BalthazarVSBotWare <ba1thazar.23730@gmail.com>
2026-03-25 00:41:22 -0600

    Update sw.js

--- journal/sw.js
+++ journal/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'malys-journal-v16';
+const CACHE = 'malys-journal-v17';
 const CORE = ['./', './index.html', './manifest.json', './icon.svg'];
 const CDN = [
   'https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=IBM+Plex+Mono:wght@300;400;500&display=swap',
